1e70e043d263a188a4f60e61a23f1ac710d17946
Hardik Kumar <hardikkumar601@gmail.com>
2024-05-01 21:46:59 +0530

    first commit

---
+++ src/pages/Auths/Login/Container2.jsx
@@ -0,0 +1,161 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Login.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaEye } from "react-icons/fa";
+import { CiCircleQuestion } from "react-icons/ci";
+import { MdOutlineLockPerson } from "react-icons/md";
+
+import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+
+const Container2 = () => {
+  const navigate = useNavigate();
+
+  const handleSignp = () => {
+    navigate("/register");
+  };
+  return (
+    <div className="card-body2">
+      <div className="logo2">
+        <svg
+          width="100px"
+          height="70px"
+          viewBox="0 0 170 190"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M158.612 52.5001L84.9999 10.0001L11.3878 52.5001L11.3878 137.5L84.9999 180L158.612 137.5L158.612 52.5001ZM105.281 81.9673L84.1608 30.3148L63.041 81.9673L28.8685 126.084L84.1608 118.548L139.453 126.084L105.281 81.9673Z"
+            fill="#458C66"
+          />
+        </svg>
+      </div>
+      <h2 className="card-title2">Welcome back!</h2>
+      <div className="details">
+        <h6>Please enter your details</h6>
+      </div>
+
+      <form>
+        <div className="mb-2">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <div style={{ position: "relative", display: "inline-block" }}>
+            <AiOutlineMail
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "10px",
+                transform: "translateY(-50%)",
+              }}
+            />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              style={{
+                paddingLeft: "2rem",
+                // paddingRight: "2rem",
+                borderRadius: "5px",
+                height: "30px",
+                fontSize: "16px",
+                width: "100%",
+                border: "1px solid #ccc",
+              }}
+            />
+            <CiCircleQuestion
+              style={{
+                position: "absolute",
+                top: "50%",
+                right: "1px",
+                transform: "translateY(-50%)",
+              }}
+            />
+          </div>
+
+          <div id="emailHelp" className="form-text">
+            Must be a Valid email address
+          </div>
+        </div>
+
+        <div className="mb-2">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            password
+          </label>
+          <div style={{ position: "relative", display: "inline-block" }}>
+            <MdOutlineLockPerson
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "10px",
+                transform: "translateY(-50%)",
+              }}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              style={{
+                paddingLeft: "2rem",
+                // paddingRight: "2rem",
+                borderRadius: "5px",
+                height: "30px",
+                fontSize: "16px",
+                width: "100%",
+                border: "1px solid #ccc",
+              }}
+            />
+            <FaEye
+              style={{
+                position: "absolute",
+                top: "50%",
+                right: "1px",
+                transform: "translateY(-50%)",
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="forgot">
+          <button className="btn"> forgot Password</button>
+        </div>
+      </form>
+
+      <a href="#" className="btn btn-primary">
+        <div className="d-grid gap-2">
+          <button className="click" type="button">
+            Log In
+          </button>
+        </div>
+      </a>
+
+      <a href="#" className="btn btn-primary">
+        <div className="d-grid gap-2">
+          <button
+            className="goggle"
+            type="button"
+            style={{ alignItems: "center" }}
+          >
+            Log In with google
+            <span style={{ marginLTop: "110rem", marginLeft: "0.5rem" }}>
+              <FcGoogle />
+            </span>
+          </button>
+        </div>
+      </a>
+      <div className="bottomtext2">
+        <center>
+          <p className="login2">
+            Don't have an account?
+            <a href="#" className="signup" onClick={handleSignp}>
+              <span> Sign Up</span>
+            </a>
+          </p>
+        </center>
+      </div>
+    </div>
+  );
+};
+
+export default Container2;
--- src/pages/Home/Welcome.jsx
+++ src/pages/Home/Welcome.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import "./Welcome.css";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-  return (
-    <div>Welcome</div>
-  )
-}
+  const Navigate = useNavigate();
 
-export default Welcome
+  const handleGetStartedClick = () => {
+    Navigate("/register");
+  };
+  const handleGetLogin = () => {
+    Navigate("/login");
+  };
+
+  return (
+    <div className="card-body">
+      <div className="logo">
+        <svg
+          width="100px"
+          height="70px"
+          viewBox="0 0 170 190"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M158.612 52.5001L84.9999 10.0001L11.3878 52.5001L11.3878 137.5L84.9999 180L158.612 137.5L158.612 52.5001ZM105.281 81.9673L84.1608 30.3148L63.041 81.9673L28.8685 126.084L84.1608 118.548L139.453 126.084L105.281 81.9673Z"
+            fill="#458C66"
+          />
+        </svg>
+      </div>
+      <h5 className="card-title">Welcome to BayzCrypt!</h5>
+
+      <p className="card-text">
+        Welcome to BayzCrypt,your gateway to secure and decentralized
+        finances.With BayzCrypt,you're in control of your assests like never
+        before.Let's get started to secure your account and ensure the safety of
+        your digital assets.
+      </p>
+
+      <div className="biglogo">
+        <svg
+          width="160"
+          height="240"
+          viewBox="-12 0  190"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M158.612 52.5001L84.9999 10.0001L11.3878 52.5001L11.3878 137.5L84.9999 180L158.612 137.5L158.612 52.5001ZM105.281 81.9673L84.1608 30.3148L63.041 81.9673L28.8685 126.084L84.1608 118.548L139.453 126.084L105.281 81.9673Z"
+            fill="#458C66"
+          />
+        </svg>
+      </div>
+      <div className="lion">
+        <a href="#" className="btn btn-primary">
+          <div className="d-grid gap-2">
+            <button
+              className="click"
+              type="button"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </button>
+          </div>
+        </a>
+        <div className="bottomtext">
+          <center>
+            <p className="login" onClick={handleGetLogin}>
+              Already have an account?<span id="btn"> Log In</span>
+            </p>
+          </center>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
